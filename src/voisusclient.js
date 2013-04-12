d01735d31e4bdb8fde8c7e0c5ce884878a80498f
Robert Chatman <bobc@labs.asti-usa.com>
2013-04-12 14:16:06 -0700

    saving state. Working notification and callback methods.

---
+++ src/voisusclient.js
@@ -0,0 +1,259 @@
+var JRPCBase = require("./jrpcbase");
+
+var obj = function (host, port, callback) {
+	this.jrpcClient = new JRPCBase(host, port, callback);
+};
+
+obj.prototype = {
+	Disconnect: function (data, encoding) {
+		this.jrpcClient.disconnect();
+	},
+
+	/** Begin Commands **/
+	ping: function (fn) {
+		var msg = this.jrpcClient.Method("ping");
+		this.jrpcClient.Send(msg, fn);
+	},
+
+	/** **/
+
+	onDisconnect : function (fn) { 
+		this.jrpcClient.onEnd(fn);
+	},
+	onResponse : function (fn) { 
+		this.jrpcClient.onData(fn);
+	},
+};
+
+module.exports = obj;
+
+var API = {
+	Connect: function (host, port, callback) 
+	{
+		if (typeof(client) !== 'undefined')
+			client.end();
+
+		client = net.connect({host: host, port: port}, function () {
+			console.log("connected to " + host);
+			callback();});
+		
+		client.on('data', function(data) { 
+		  console.log(data.toString());
+		});
+
+		client.on('end', function() {
+		  console.log('client disconnected');
+		});
+	},
+	Data: function (callback) 
+	{
+		if (typeof(client) === 'undefined')
+			return callback("Not connected to VoisusMain");
+
+		client.on('data', callback);
+	},
+	End: function (callback) 
+	{
+		if (typeof(client) === 'undefined')
+			return callback("Not connected to VoisusMain");
+
+		if (typeof(callback) === 'undefined')
+			client.end();
+		else
+			client.on('end', callback);
+	},
+	JRPCObj: function (_method, _params, _id)
+	{
+		if (typeof(_method) !== 'string')
+			return null;
+		
+		if (typeof(_params) === 'undefined')
+			_params = {};
+
+		if (typeof(_id) === 'undefined')
+			_id = null;
+
+		return { jsonrpc: "2.0", id: _id, method: _method, params: _params};
+	},
+	JRPCString: function (jrpcObj)
+	{
+		return jsonify.stringify(jrpcObj) + "\0"; 
+	},
+	Send: function (jrpcObj, callback)
+	{
+		if (typeof(client) === 'undefined')
+			return callback("Not connected to VoisusMain");
+
+		client.write(jsonify.stringify(jrpcObj), 'UTF8', callback)
+	},
+	commands: {
+		ping: function (id, callback) {
+			var obj = API.JRPCObj("ping",
+				{},
+				id);
+			API.Send(obj, callback);
+		}, 
+		connect: function (addr, id, callback) {
+			var obj = API.JRPCObj("connect",
+				{ addr: addr },
+				id);
+			API.Send(obj, callback);
+		},
+		keep_alive: function (id, callback) {
+			var obj = API.JRPCObj("keep_alive",
+				{},
+				id);
+			API.Send(obj, callback);
+		},
+		disconnect: function (id, callback) {
+			var obj = API.JRPCObj("disconnect",
+				{},
+				id);
+			API.Send(obj, callback);
+		},
+		get_operators: function (id, callback) {
+			var obj = API.JRPCObj("get_operators",
+				{},
+				id);
+			API.Send(obj, callback);
+		},
+		get_assets: function (id, callback) {
+			var obj =  API.JRPCObj("get_assets",
+				{},
+				id);
+			API.Send(obj, callback);
+		},
+		get_connection: function (id, callback) {
+			var obj = API.JRPCObj("get_connection",
+				{},
+				id);
+			API.Send(obj, callback);
+		},
+		get_client_name: function (id, callback) {
+			var obj = API.JRPCObj("get_client_name",
+				{},
+				id);
+			API.Send(obj, callback);
+		},
+		get_curr_role: function (id, callback) {
+			var obj = API.JRPCObj("get_curr_role",
+				{},
+				id);
+			API.Send(obj, callback);
+		},
+		get_analyzer_results: function (id, callback) {
+			var obj = API.JRPCObj("get_analyzer_results",
+				{},
+				id);
+			API.Send(obj, callback);
+		},
+		get_asset_nets: function (id, callback) {
+			var obj = API.JRPCObj("get_asset_nets",
+				{},
+				id);
+			API.Send(obj, callback);
+		},
+		get_network_settings: function (id, callback) {
+			var obj = API.JRPCObj("get_network_settings",
+				{},
+				id);
+			API.Send(obj, callback);
+		},
+		get_headset_settings: function (id, callback) {
+			var obj = API.JRPCObj("get_headset_settings",
+				{},
+				id);
+			API.Send(obj, callback);
+		},
+		get_roles: function (id, callback) {
+			var obj = API.JRPCObj("get_roles",
+				{},
+				id);
+			API.Send(obj, callback);
+		},
+		get_vehicles: function (id, callback) {
+			var obj = API.JRPCObj("get_vehicles",
+				{},
+				id);
+			API.Send(obj, callback);
+		},
+		get_transmitting: function (id, callback) {
+			var obj = API.JRPCObj("get_transmitting",
+				{},
+				id);
+			API.Send(obj, callback);
+		},
+		get_versions: function (id, callback) {
+			var obj = API.JRPCObj("get_versions",
+				{},
+				id);
+			API.Send(obj, callback);
+		},
+		start_analyzer: function (id, callback) {
+			var obj = API.JRPCObj("start_analyzer",
+				{},
+				id);
+			API.Send(obj, callback);
+		},
+		stop_analyzer: function (id, callback) {
+			var obj = API.JRPCObj("stop_analyzer",
+				{},
+				id);
+			API.Send(obj, callback);
+		},
+		set_radiomon: function (/* Integer 1 on 0 off */ status, id, callback) {
+			var obj = API.JRPCObj("set_radiomon",
+				{status: status},
+				id);
+			API.Send(obj, callback);
+		},
+		autotune_radio: function (id, callback) {
+			var obj = API.JRPCObj("autotune_radio",
+				{},
+				id);
+			API.Send(obj, callback);
+		},
+		set_vehicle: function (vehicle_id, id, callback) {
+			var obj = API.JRPCObj("set_vehicle",
+				{id: vehicle_id},
+				id);
+			API.Send(obj, callback);
+		},
+		set_ptt: function (/* number */ ptt, id, callback) {
+			var obj = API.JRPCObj("set_ptt",
+				{ptt : ptt},
+				id);
+			API.Send(obj, callback);
+		},
+		set_role: function (role_id, id, callback) {
+			var obj = API.JRPCObj("set_role",
+				{id: role_id},
+				id);
+			API.Send(obj, callback);
+		},
+		set_asset: function (id, callback) {
+			var obj = API.JRPCObj("set_asset",
+				{},
+				id);
+			API.Send(obj, callback);
+		},
+		set_client_name: function (/* string */ name, id, callback) {
+			var obj = API.JRPCObj("set_client_name", 
+				{name: name}, 
+				id);
+			API.Send(obj, callback);
+		},
+		set_test_mode: function (mode, id, callback) {
+			var obj = API.JRPCObj("set_test_mode",
+				{mode: mode},
+				id);
+			API.Send(obj, callback);
+		},
+		set_world_pos: function (/* number */ xCoord, /* number */ yCoord, /* number */ zCoord, id, callback) {
+			var obj = API.JRPCObj("set_world_pos", 
+				{xcoord: xCoord, ycoord: yCoord, zcoord: zCoord}, 
+				id);
+			API.Send(obj, callback);
+		}
+	},
+}
--- test/voisus-test.js
+++ test/voisus-test.js
@@ -1,74 +1,30 @@
 var should = require('should');
-var voisus = require('../src/voisus');
+var vc = require('../src/voisusclient');
 
-describe('JRPC Object Components: ', function() {
-	it("Should have accept a method as an argument.", function()
-	{
-		var tmpV = voisus.JRPCObj("testMethod");		
-		tmpV.method.should.equal("testMethod");
-		
-		var tmpV = voisus.JRPCObj("testMethod2");		
-		tmpV.method.should.equal("testMethod2");
-	});
+var test = {
+	host: "10.26.0.117", 
+	port: 51234
+}
 
-	it("Should have an empty params set and id should be null by default.", function()
-	{
-		var tmpV = voisus.JRPCObj("testMethod");		
+describe.skip('Voisus Client: ', function () {
+	it("Should connect at initialization, and provide disconnect", function (done) {
 
-		Object.keys(tmpV.params).length.should.equal(0);
-		should.not.exist(tmpV.id);
-	});
+		var client = new vc(test.host, test.port, function () {
+			client.onDisconnect(function (data) { done(); });
+			client.onResponse(function (data) {console.log(data.toString());});
+			
+			client.ping(function (data) { console.log(data.toString());});
 
-	it("Should have the ability to pass parameters in as second argument.", function()
-	{
-		var tmpV = voisus.JRPCObj("testMethod", {test1: 1, test2: 2});		
-
-		Object.keys(tmpV.params).length.should.equal(2);
-
-		should.exist(tmpV.params.test1);
-		tmpV.params.test1.should.equal(1);
-
-		should.exist(tmpV.params.test2);
-		tmpV.params.test2.should.equal(2);
-
-		should.not.exist(tmpV.id);
-	});
-
-	it("Should have the ability to pass new id in as third argument.", function()
-	{
-		var tmpV = voisus.JRPCObj("testMethod", {});	
-		
-		should.not.exist(tmpV.id);
-
-		tmpV = voisus.JRPCObj("testMethod", {}, 42);
-		
-		should.exist(tmpV.id);	
-		tmpV.id.should.equal(42);		
-	});
-
-	it("Should provide means of converting object to string.", function()
-	{
-		var tmpV = voisus.JRPCObj("testMethod");	
-
-		voisus.JRPCString(tmpV).should.equal('{"jsonrpc":"2.0","id":null,"method":"testMethod","params":{}}\0');
-	});
-
-	it("Should always have trailing \0 on generated json strings.", function()
-	{
-		var tmpV = voisus.JRPCObj("testMethod");	
-
-		var str = voisus.JRPCString(tmpV);
-
-		str[str.length - 1].should.equal("\0");
+			client.Disconnect();
+		})
 	});
 });
 
-describe('Voisus Commands: ', function() {
+describe.skip('Voisus Commands: ', function() {
 	it("Should provide Ping", function (done)
 	{
-		voisus.Connect("10.26.0.117", "51234", function() {
-			voisus.Data(function (data) { console.log(data); });
-			voisus.Commands.ping(0, function () {							
+		voisus.Connect(test.host, test.port, function() {		
+			voisus.commands.ping(0, function () {		
 				done();				
 			});
 		});		
@@ -76,9 +32,8 @@ describe('Voisus Commands: ', function() {
 
 	it("Should provide Connect", function (done)
 	{
-		voisus.Connect("10.26.0.117", "51234", function() {
-			voisus.Data(function (data) { console.log(data); });
-			voisus.Commands.connect("10.26.4.111", 0, function () {	
+		voisus.Connect(test.host, test.port, function() {			
+			voisus.commands.connect("10.26.4.111", 0, function () {	
 				done();				
 			});
 		});		
