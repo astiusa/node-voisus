180e926c7ff4cd9300d5dcb0f5312d114ad7b87e
Ross Kukulinski <ross.kukulinski@asti-usa.com>
2013-06-20 17:01:09 -0700

    Added working getradios call

--- src/hapi.js
+++ src/hapi.js
@@ -1,4 +1,5 @@
 var rest = require("restler");
+var async = require('async');
 
 var obj = function (host) {
   this.host = host;
@@ -10,9 +11,28 @@ obj.prototype = {
   request: function(url, type, data, cb) {
     if (typeof data === 'function') {
       cb = data;
+      data = null;
     }
     if (type === 'get') {
       rest.get(url).on('complete', function(result) {
+        if (result instanceof Error) {
+          console.log('Error: '+ result.message);
+          return cb(result);
+        }
+        cb(null, result);
+      });
+    }
+    else if (type === 'post') {
+      rest.post(url).on('complete', function(result) {
+        if (result instanceof Error) {
+          console.log('Error: '+ result.message);
+          return cb(result);
+        }
+        cb(null, result);
+      });
+    }
+    else if (type === 'put') {
+      rest.put(url, data, {parser: rest.parsers.json}).on('complete', function(result) {
         if (result instanceof Error) {
           console.log('Error: '+ result.message);
           return cb(result);
@@ -33,13 +53,67 @@ obj.prototype = {
       cb(err, result);
     });
   },
+  getAboutMe: function(cb) {
+    var url = this.url+'aboutme/';
+    this.request(url, 'get', function(err, result) {
+      cb(err, result);
+    });
+  },
   getPerfmon: function(cb) {
     var url = this.url+'perfmon/';
     this.request(url, 'get', function(err, result) {
       cb(err, result);
     });
+  },
+  getRunlevel: function(cb) {
+    var url = this.url+'syspower/';
+    this.request(url, 'get', function(err, result) {
+      if (result) {
+        return cb(err, result.runlevel);
+      }
+      else {
+        return cb(err);
+      }
+    });
+  },
+  reboot: function(cb) {
+    var url = this.url+'syspower/reboot/';
+    this.request(url, 'post', function(err, result) {
+      cb(err, result);
+    });
+  },
+  getNumClients: function(cb) {
+    var url = this.url+'streams/clientmon/';
+    var request = this.request;
+    request(url, 'post', function(err, result) {
+      if (err || !result || !result.state) {
+        return cb(err);
+      }
+      cb(null, result.state.length);
+    });
+  },
+  getNumRadios: function(cb) {
+    var url = this.url+'radiomon/subscriptions/';
+    var request = this.request;
+    var sub;
+    request(url, 'post', function(err, result) {
+      if (err) {
+        return cb(err);
+      }
+      sub = result.self;
+      var req = {payload:{"__asti_message":"start-stats"}};
+      request(sub, 'put', JSON.stringify(req), function(err, result) {
+        if (err) {
+          return cb(err);
+        }
+        result = JSON.parse(result);
+        result = JSON.parse(result.payload[0]);
+        cb(null, result.radios.length);
+      });
+      //console.log(result.state);
+      //console.log(result.state.length);
+    });
   }
-
 };
 
 module.exports = obj;
--- test/hapi-test.js
+++ test/hapi-test.js
@@ -9,12 +9,12 @@ var test = {
 describe('Voisus HAPI: ', function () {
   describe('Basic Functionality: ', function () {
 
-    it ("should accept a host", function () {
+    it("should accept a host", function () {
       var h = new hapi(test.host);
       should.exist(h);
     });
 
-    it ("should get the api version", function(done) {
+    it("should get the api version", function(done) {
       var h = new hapi(test.host);
       h.getAPIVersion(function(err, ver) {
         should.not.exist(err);
@@ -24,7 +24,7 @@ describe('Voisus HAPI: ', function () {
       });
     });
 
-    it ('should get the server version', function(done) {
+    it('should get the server version', function(done) {
       var h = new hapi(test.host);
       h.getVersion(function(err, ver) {
         should.not.exist(err);
@@ -42,10 +42,28 @@ describe('Voisus HAPI: ', function () {
       });
     });
 
+    it('should get info aboutme', function(done) {
+      var h = new hapi(test.host);
+      h.getAboutMe(function(err, about) {
+        should.not.exist(err);
+        should.exist(about);
+        should.exist(about.id);
+        about.id.should.equal('aboutme');
+        should.exist(about.contact_phone1);
+        about.contact_phone1.should.eql('703-471-2104');
+        should.exist(about.contact_email);
+        about.contact_email.should.eql('sales@asti-usa.com');
+        should.exist(about.contact_name);
+        about.contact_name.should.eql('ASTi Sales');
+        should.exist(about.description);
+        about.description.should.eql('HearVoisus Server');
+        done();
+      });
+    });
   });
 
   describe('Performance: ', function() {
-    it ("should get perfmon data", function(done) {
+    it("should get perfmon data", function(done) {
       var h = new hapi(test.host);
       h.getPerfmon(function(err, stats) {
         should.not.exist(err);
@@ -59,6 +77,53 @@ describe('Voisus HAPI: ', function () {
         should.exist(stats.stats.swapfree);
         should.exist(stats.stats.cpu0);
         should.exist(stats.stats.eth0);
+        done();
+      });
+    });
+  });
+
+  describe('System Power: ', function() {
+    it('should get the run level', function(done) {
+      var h = new hapi(test.host);
+      h.getRunlevel(function(err, runlevel) {
+        should.not.exist(err);
+        should.exist(runlevel);
+        runlevel.should.eql('3');
+        done();
+      });
+    });
+
+    // reboot is broken, never returns data
+    it.skip('should reboot the box', function(done) {
+      var h = new hapi(test.host);
+      h.reboot(function(err) {
+        should.not.exist(err);
+        done();
+      });
+    });
+  });
+  describe('Client Mon: ', function() {
+    it('should get the number of clients', function(done) {
+      var h = new hapi(test.host);
+      h.getNumClients(function(err, numclients) {
+        should.not.exist(err);
+        should.exist(numclients);
+        numclients.should.eql(1);
+        done();
+      });
+    });
+  });
+
+  describe('Radio Mon: ', function() {
+    it('should get the number of radios', function(done) {
+      var h = new hapi(test.host);
+      h.getNumRadios(function(err, numradios) {
+        should.not.exist(err);
+        should.exist(numradios);
+        numradios.should.eql(5);
+        //onsole.log(numradios[0]);
+        //console.log(JSON.parse(numradios[0]));
+        //numradios.should.eql(4);
         done();
       });
     });
